082b124b377bc2436c28d4b3014acd0796b7611e
Daniel <denial821124@gmail.com>
2025-07-01 22:52:38 +0800

    重構 TestController，移除未使用的 Try 方法，整理建構函式

--- PizzaKing/Controllers/TestController.cs
+++ PizzaKing/Controllers/TestController.cs
@@ -2,36 +2,26 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PizzaKing.Models;
 
-
-
 namespace PizzaKing.Controllers;
 
 
 public class TestController : Controller
 {
-    public string Try()
+    public TestController(ILogger<TestController> logger)
     {
-        // This is a placeholder for the Index action method.
-        // You can return a view or any other result as needed.
-        return "Hello, this is the TestController's Try method!";
+        _logger = logger;
     }
-    // private readonly ILogger<TestController> _logger;
 
-    // public TestController(ILogger<TestController> logger)
-    // {
-    //     _logger = logger;
-    // }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-    // [HttpGet]
-    // public IActionResult Index()
-    // {
-    //     return View();
-    // }
-
-    // [HttpPost]
-    // public IActionResult Index(string name)
-    // {
-    //     _logger.LogInformation("TestController.Index called with name: {Name}", name);
-    //     return View();
-    // }
+    [HttpPost]
+    public IActionResult Index(string name)
+    {
+        _logger.LogInformation("TestController.Index called with name: {Name}", name);
+        return View();
+    }
 }
